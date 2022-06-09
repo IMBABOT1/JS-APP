04a25d5285905bb8a8824bdf95f53deaa6f3191e
unknown <zlovred12@gmail.com>
2022-06-09 18:30:20 +0300

    web count

--- JoinUs/hw.js
+++ JoinUs/hw.js
@@ -3,10 +3,27 @@ const { error } = require("console");
 const mysql = require("mysql2");
 
 var express = require('express');
+
 var app = express();
+var mysqll = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "join_us",
+    password: "Gg2A7Vyz"
+  });
+
 
 app.get("/", function(req, res){
-    res.send("You've Reached The Home Page!")
+    var q = "SELECT COUNT(*) as count FROM users"
+    connection.query(q, function(err, results){
+        if (err) throw err;
+        var count = results[0].count;
+        res.send("We have " + count + " users in our db")
+    });
+    // res.send("You've Reached The Home Page!")
 });
 
 app.get("/joke", function(req, res){
@@ -18,6 +35,15 @@ app.get("/random", function(req, res){
     var num = Math.floor(Math.random() * 10) + 1;
     res.send("Your lucky number is " + num);
 });
+
+
+
+// connection.end(function(err) {
+//     if (err) {
+//       return console.log("Ошибка: " + err.message);
+//     }
+//     console.log("Подключение закрыто");
+//   });
    
 
 app.listen(8080, function(){
